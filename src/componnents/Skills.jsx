c175859e97e956df693706b5729de79a92541a83
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-10-10 21:29:18 +0200

    modified

--- src/componnents/Skills.jsx
+++ src/componnents/Skills.jsx
@@ -7,6 +7,8 @@ import sass from '../images/sass.png'
 import react from '../images/react.png'
 import ts from '../images/typescript.png'
 import redux from '../images/redux.png'
+import next from '../images/next.png';
+
 const Skills = () => {
   return (
     <div className='skills' id='skills'>
@@ -19,31 +21,35 @@ const Skills = () => {
           <span>HTML</span>
         </div>
         <div className="card">
-          <img src={css} alt="html" />
+          <img src={css} alt="css" />
           <span>CSS</span>
         </div>
         <div className="card">
-          <img src={js} alt="html" />
+          <img src={js} alt="js" />
           <span>JavaScript</span>
         </div>
         <div className="card">
-          <img src={ts} alt="html" />
+          <img src={ts} alt="ts" />
           <span>TypeScript</span>
         </div>
         <div className="card">
-          <img src={react} alt="html" />
+          <img src={react} alt="react" />
           <span>React</span>
         </div>
         <div className="card">
-          <img src={redux} alt="html" />
+          <img src={redux} alt="redux" />
           <span>Redux</span>
         </div>
         <div className="card">
-          <img src={sass} alt="html" />
+          <img src={next} alt="next" />
+          <span>Next</span>
+        </div>
+        <div className="card">
+          <img src={sass} alt="sass" />
           <span>SASS</span>
         </div>
         <div className="card">
-          <img src={github} alt="html" />
+          <img src={github} alt="github" />
           <span>Github</span>
         </div>
       </div>
