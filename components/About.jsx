facf829b54c95b89bb98916e9b716d26d1562f72
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-10-27 22:10:03 +0200

    modified

--- components/About.jsx
+++ components/About.jsx
@@ -7,13 +7,14 @@ const About = () => {
       <div className="container">
         <div className="left">
           <h3>
-            Hi. I'm Abdalrhman, nice to meet you. Please take a look around.
+            Hi. I&apoa;m Abdalrhman, nice to meet you. Please take a look
+            around.
           </h3>
         </div>
         <div className="right">
           <p>
-            I am passionate about building excellent software that improves the
-            lives of those around me. I specialize in creating software for
+            I&apoa;m passionate about building excellent software that improves
+            the lives of those around me. I specialize in creating software for
             clients ranging from individuals and small-businesses all the way to
             large enterprise corporations. What would you do if you had a
             software expert available at your fingertips?
